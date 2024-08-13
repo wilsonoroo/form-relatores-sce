fc4b92990edafc0d3e28762d7869c188eddda99b
Wilson <wilsonoroo@gmail.com>
2024-08-13 12:00:12 -0400

    Conexion netlify

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export',
+    // Other configurations...
+  };
+  
+  module.exports = nextConfig;
+  
--- next.config.mjs
+++ next.config.mjs
@@ -2,3 +2,15 @@
 const nextConfig = {};
 
 export default nextConfig;
+
+
+// next.config.mjs
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     output: 'export',
+//     // otras configuraciones aquí si las tienes
+// }
+  
+// export default nextConfig
+  
